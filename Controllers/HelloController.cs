e74b2cd28db8946b71e27ce7c05d6251db2baefc
Ben Lopez <benswengineer@outlook.com>
2025-03-05 13:29:17 -0500

    initial commit

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Platform.Challenge.Helloer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        private readonly IConfiguration config;
+        private readonly Dictionary<string, string> Helloers = new Dictionary<string, string>()
+        {
+            {"MexDev", "Hola Mundo" },
+            {"QuebecDev", "Bonjour le monde" },
+            { "MontrealDev","Hello World" }
+        };
+
+        public HelloController(IConfiguration configuration)
+        {
+            this.config = configuration;
+        }
+        public ActionResult Index()
+        {
+            var role = GetDeveloperRole();
+            return Ok(Helloers[role]);
+        }
+
+        private string GetDeveloperRole()
+        {
+            var value = this.config["DeveloperRole"];
+            return value != null ? value : "Undefined";
+        }
+    }
+}
